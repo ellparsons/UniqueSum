db89e9b4d0ff714b559ba875dd948374016f8c3f
ellparsons <45197919+ellparsons@users.noreply.github.com>
2018-12-19 17:31:08 +0000

    Add files via upload

---
+++ src/uniqueSum/UniqueSum.java
@@ -0,0 +1,15 @@
+package uniqueSum;
+
+public class UniqueSum {
+
+	public static void main(String[]args)
+	{
+		uniqueSum (1,2,3);	
+	}
+	
+	public static void uniqueSum(int num1, int num2, int num3)
+	{
+		
+		System.out.println(num1+num2+num3);		
+	}
+}
